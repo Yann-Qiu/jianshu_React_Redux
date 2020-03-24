150c6f7a0b1d351e4b43b9fbde11d47e47db38e1
qs4091 <56961747+qs4091@users.noreply.github.com>
2020-03-24 11:43:32 +0100

    add detail page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import { Page,Button,DetailWrapper,DetailTitle,DetailText,DetailWriter } from './style.js';
 
 class Detail extends Component{
 	
 	render(){
 		return (
-			<div>Detail</div>
+			<Page>
+				<Button>
+					<div className="like">
+						<span className="iconfont">&#xe61c;</span>	
+					</div>
+					<div className="word">747赞</div>
+					<div className="prime">
+						<span className="iconfont">&#xe8a3;</span>
+					</div>
+					<div className="word">赞赏</div>
+				</Button>
+				<DetailWrapper>
+					<DetailTitle>
+						Title
+					</DetailTitle>
+					<DetailWriter>
+						<img 
+							className="pic" 
+							src="https://upload.jianshu.io/users/upload_avatars/10934411/2b95aa67-88ea-4ad1-b069-5f784b074086.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" 
+							alt="img"
+						/>
+						<div className="detailInfo">
+							<div className="name">Name</div>
+							<div className="desc">
+								<span className="descItem diamond">
+									<span className="iconfont">&#xe625;</span>
+									1
+								</span>
+								<span className="descItem">
+									2020.02.08 14:17:12
+								</span>
+								<span className="descItem">
+									字数 450
+								</span>
+								<span className="descItem">
+									阅读 62,495
+								</span>
+							</div>
+						</div>
+					</DetailWriter>
+					<DetailText>
+						TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+						TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+						TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+						TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+						TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText
+					</DetailText>
+				</DetailWrapper>
+			</Page>
 		)
 	}
 	
---
+++ src/pages/detail/style.js
@@ -0,0 +1,122 @@
+import styled  from 'styled-components';
+
+
+export const Page = styled.div`
+	background-color: #f9f9f9;
+	width : 100%;
+	min-height: 960px;
+	padding-top: 10px;
+`
+
+export const Button = styled.div`
+	position:fixed;
+	width:55px;
+	left:185px;
+	top: 200px;
+	.word{
+		height: 19px;
+		font-size: 14px;
+	    text-align: center;
+	    color: #969696;
+	    margin-top:5px;
+	    margin-bottom:10px;
+	    cursor : pointer;
+	}
+	.like{
+		cursor : pointer;
+		height: 48px;
+		width: 48px;
+		text-align: center;
+		line-height: 48px;
+		background: #fff;
+		border-radius:50%;
+		.iconfont{
+			font-size: 18px;
+			color: #969696;
+		}
+	}
+	.prime{
+		cursor : pointer;
+		height: 48px;
+		width: 48px;
+		text-align: center;
+		line-height: 48px;
+		background: #fff;
+		border-radius:50%;
+		.iconfont{
+			font-size: 18px;
+			color: #969696;
+		}
+	}
+`
+
+export const DetailWrapper = styled.div`
+	padding: 24px;
+	width : 960px;
+	margin: 0 auto;
+	background-color: #fff;
+	min-height: 800px;
+`
+
+export const DetailTitle = styled.div`
+	font-size: 30px;
+    font-weight: 700;
+    color: #404040;
+    margin-top: 0;
+    margin-bottom: .5em;	
+`
+
+export const DetailText = styled.div`
+	margin-bottom: 20px;
+	margin-top: 0;
+	color: #404040;	
+	font-weight: 400;
+    line-height: 40px;
+    font-size: 16px;
+`
+
+export const DetailWriter = styled.div`
+	margin-bottom: 32px;
+	height:48px;
+    width :100%;
+    box-sizing: border-box;
+    overflow:hidden;
+    .pic{
+    	float:left;
+    	display: block;
+    	border-radius: 50%;
+	    border: 1px solid #eee;
+	    min-width: 48px;
+	    min-height: 48px;
+	    width: 48px;
+	    height: 48px;
+    }
+    .detailInfo{
+    	float:left;
+    	margin-left:15px;
+    	box-sizing:border-box;
+    	.name{
+			font-size: 16px;
+    		font-weight: 500;
+    		margin-top: 3px;
+    		margin-bottom: 6px;
+    	}
+    	.desc{
+    		.descItem{
+    			margin-right: 10px;
+    			font-size: 13px;
+    			color: #969696;
+    			&.diamond{
+    				color: #ec7259;
+    				font-size: 13px;
+    				margin-right: 10px;
+    				.iconfont{
+    					margin-right: 3px;
+    				}
+    			}
+    		}
+    	}
+    }
+`
+
+
